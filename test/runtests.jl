8bb28116e229ea41189192c80017f9c4920ded08
Elena Gomez del Pozo <elena.gomez.dp@gmail.com>
2026-03-20 11:36:16 +0100

    cleaning perfecting couples branch

--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-Pkg.instantiate()
-
-include("infuser_test.jl")
-include("extrapolator_test.jl")
-include("regressor_test.jl")
-include("parameter_tuner_test.jl")
-include("plotter_test.jl")
