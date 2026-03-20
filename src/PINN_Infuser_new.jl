d364a32c41159d650ec8f134b3ed2e99c445bc39
Elena Gomez del Pozo <elena.gomez.dp@gmail.com>
2026-03-20 16:17:18 +0100

    restructuring of how pinn infuser sees losses

--- src/PINN_Infuser_new.jl
+++ src/PINN_Infuser_new.jl
@@ -2,7 +2,6 @@ module PINNInfuser_new
 
 __precompile__(false)   # fixes the rrule/ChainRules conflict
 
-# using LuxCUDA, CUDA
 using Lux
 using StableRNGs, ComponentArrays, LinearAlgebra
 using Optimization, OptimizationOptimisers
@@ -12,11 +11,10 @@ using OrdinaryDiffEq: Vern7, Tsit5, Rodas4
 using Printf
 using Plots, Zygote, Statistics
 
-export PINN_Infuser_new
+include("Losses.jl")
+using .Losses
 
-#const gdev = gpu_device()   # LuxDeviceUtils GPU device
-#const cdev = cpu_device()   # LuxDeviceUtils CPU device
-#_gpu_available() = CUDA.functional() 
+export PINN_Infuser_new
 
 """
 
@@ -54,374 +52,106 @@ function PINN_Infuser_new(
     nn::Lux.Chain,
     training_steps::AbstractRange,
     target_data::AbstractMatrix{Float64};
-    processor,
-    early_stopping::Bool = true,
+    active::Vector{String},
+    config::NamedTuple,
     nn_output_weight::Float64 = 1.0,
-    data_weight::Float64 = 1.0,
-    deriv_weight::Float64 = 1.0,
-    physics_weight::Float64 = 1.0,
-    zm_weight::Float64 = 1.0,
-    neg_weight::Float64 = 1.0,
-    periodic_weight::Float64 = 1.0,
-    mass_conservation_weight::Float64 = 1.0,
     optimizer = ADAM,
     learning_rate::Float64 = 1e-3,
     reltol::Float64 = 1e-6,
     abstol::Float64 = 1e-6,
     dtmax = Inf,
     iters::Int = 1000,
+    early_stopping::Bool = true,
     plot_every::Int = 10,
     early_stop_warmup::Int = 50,
     early_stop_window::Int = 10,
-    rng::StableRNG = StableRNG(5958),
-    nn_vars::Union{Nothing,Vector{Int}} = nothing,
-    data_vars::Union{Nothing,Vector{Int}} = nothing,
-    physics_vars::Union{Nothing,Vector{Int}} = nothing,
-    deriv_vars::Union{Nothing,Vector{Int}} = nothing,
-    zm_vars::Union{Nothing,Vector{Int}} = nothing,
-    neg_vars::Union{Nothing,Vector{Int}} = nothing
+    rng::StableRNG = StableRNG(5958)
 )::Tuple{Any,Any, Any, Any}
-    if processor == "cpu"
-        U_MEAN = vec(mean(target_data, dims = 1))
-        U_STD = vec(std(target_data, dims = 1)) .+ 1e-6
-        data_norm = (target_data .- U_MEAN') ./ U_STD'
 
-        p_NN, st = Lux.setup(rng, nn)
-        p_NN = 1e-3 * ComponentVector{Float64}(p_NN)
+    U_MEAN = vec(mean(target_data, dims = 1))
+    U_STD = vec(std(target_data, dims = 1)) .+ 1e-6
+    data_norm = (target_data .- U_MEAN') ./ U_STD'
 
-        function cpu_pinn_ode!(du, u, p_NN, t)
-            nn_output = nn(u, p_NN, st)[1]
-            ode_problem.f(du, u, ode_params, t)
-            for (k, i) in enumerate(nn_vars)
-                du[i] += nn_output_weight * nn_output[k]
-            end    
-        end
+    p_NN, st = Lux.setup(rng, nn)
+    p_NN = 1e-3 * ComponentVector{Float64}(p_NN)
 
-        function cpu_predict(p_NN)
-            prob = ODEProblem(
-                (du, u, p, t) -> cpu_pinn_ode!(du, u, p, t), 
-                ode_problem.u0,
-                ode_problem.tspan,
-                p_NN,
-            )
-            temp_sol = solve(
-                prob, 
-                Vern7(),
-                saveat=training_steps,
-                dtmax=dtmax,
-                reltol=reltol,
-                abstol=abstol
-            )
-            return temp_sol
-        end
+    function pinn_ode!(du, u, p_NN, t)
+        nn_output = nn(u, p_NN, st)[1]
+        ode_problem.f(du, u, ode_params, t)
+        for (k, i) in enumerate(nn_vars)
+            du[i] += nn_output_weight * nn_output[k]
+        end    
+    end
 
-        function cpu_compute_nn_contributions(sol, p_NN)
-            t = sol.t
-            u_mat = hcat(sol.u...)'  # (time × variables)
-            # Return (time × nn_vars) matrix instead of summing
-            nn_contrib = zeros(length(t), length(nn_vars))
-            for (k, i) in enumerate(nn_vars)
-                for ti in 1:length(t)
-                    nn_contrib[ti, k] = nn_output_weight * nn(u_mat[ti, :], p_NN, st)[1][k]
-                end
-            end
-            return nn_contrib  # (time × nn_vars)
-        end
+    function predict(p_NN)
+        prob = ODEProblem(
+            (du, u, p, t) -> cpu_pinn_ode!(du, u, p, t), 
+            ode_problem.u0,
+            ode_problem.tspan,
+            p_NN,
+        )
+        temp_sol = solve(
+            prob, 
+            Vern7(),
+            saveat=training_steps,
+            dtmax=dtmax,
+            reltol=reltol,
+            abstol=abstol
+        )
+        return temp_sol
+    end
 
-        function cpu_data_loss(pred_norm, data_norm, data_vars)
-            return data_weight * sum(mean(abs2, pred_norm[:, j] .- data_norm[:, j]) for j in data_vars)
-        end
-
-        function cpu_physics_loss(pred_mat, training_steps, ode_params, physics_vars)
-            dt = training_steps[2] - training_steps[1]
-            n  = size(pred_mat, 1)
-            l  = zero(eltype(pred_mat))
-
-            # scale by std of each variable's derivative to normalize units
-            du_scale = vec(std(diff(pred_mat, dims=1) ./ dt, dims=1)) .+ 1e-6
-
-            for i in 2:n-1
-                t = training_steps[i]
-                u = pred_mat[i, :]
-
-                du_ode = zeros(eltype(u), length(u))
-                ode_problem.f(du_ode, u, ode_params, t)
-                du_num = (pred_mat[i+1, :] .- pred_mat[i-1, :]) ./ (2dt)
-
-                for j in physics_vars
-                    l += abs2((du_num[j] - du_ode[j]) / du_scale[j])  # ← normalized
-                end
-            end
-
-            return physics_weight * l / (n-2)
-        end
-
-        function cpu_mass_conservation_loss(pred_mat, training_steps)
-            dt = training_steps[2] - training_steps[1]
-
-            # integrate each flow over the cycle (trapezoidal rule)
-            Qav  = pred_mat[:, 7]
-            Qmv  = pred_mat[:, 8]
-            Qs   = pred_mat[:, 9]
-            Qsv  = pred_mat[:, 10]
-
-            # total volume per cycle for each flow
-            ∫Qav  = sum((Qav[1:end-1]  .+ Qav[2:end])  ./ 2) * dt
-            ∫Qmv  = sum((Qmv[1:end-1]  .+ Qmv[2:end])  ./ 2) * dt
-            ∫Qs   = sum((Qs[1:end-1]   .+ Qs[2:end])   ./ 2) * dt
-            ∫Qsv  = sum((Qsv[1:end-1]  .+ Qsv[2:end])  ./ 2) * dt
-
-            # all four must carry the same total volume around the loop
-            mean_flow = (∫Qav + ∫Qmv + ∫Qs + ∫Qsv) / 4
-
-            l = abs2(∫Qav - mean_flow) +
-                abs2(∫Qmv - mean_flow) +
-                abs2(∫Qs  - mean_flow) +
-                abs2(∫Qsv - mean_flow)
-
-            return mass_conservation_weight * l
-        end
-
-        function cpu_zero_mean_loss(sol, p_NN, zm_vars)
-            t = sol.t
-            u_mat = hcat(sol.u...)'
-            l = 0.0
-            for (k, i) in enumerate(nn_vars)
-                if i in zm_vars
-                    nn_over_time = [nn(u_mat[ti, :], p_NN, st)[1][k] for ti in 1:length(t)]
-                    l += abs2(sum(nn_over_time))  # sum should be zero
-                end
-            end
-            return zm_weight * l
-        end
-
-        function cpu_negativity_loss(pred_mat, neg_vars)
-            l = 0.0
-            for j in neg_vars
-                l += abs2(sum(min.(pred_mat[:, j], 0.0)))  # only penalizes negative values
-            end
-            return neg_weight * l
-        end 
-
-        function cpu_firstderiv_loss(pred_mat, target_data, firstderiv_vars)
-            dt = training_steps[2] - training_steps[1]
-            l = 0.0
-            for j in firstderiv_vars
-                dpred   = diff(pred_mat[:, j],        dims=1) ./ dt
-                dtarget = diff(target_data[:, j],     dims=1) ./ dt
-                l += mean(abs2, dpred .- dtarget)
-            end
-            return deriv_weight * l
-        end
-
-        function cpu_periodicity_loss(pred_mat)
-            l = 0.0
-            for j in 1:10
-                l += abs2(pred_mat[1, j] - pred_mat[end, j])
-            end
-            return periodic_weight * l
-        end
-
-        function cpu_loss(p_NN)
-            pred = cpu_predict(p_NN)
-            pred_mat = hcat(pred.u...)'
-            pred_norm = (pred_mat .- U_MEAN') ./ U_STD'
-            L_data = cpu_data_loss(pred_norm, data_norm, data_vars)
-            L_phy = cpu_physics_loss(pred_mat, training_steps, ode_params, physics_vars)   
-            L_zm = cpu_zero_mean_loss(pred, p_NN, zm_vars)
-            L_deriv = cpu_firstderiv_loss(pred_mat, target_data, deriv_vars)   
-            L_neg = cpu_negativity_loss(pred_mat, neg_vars)  
-            L_periodic = cpu_periodicity_loss(pred_mat)
-            L_mass = cpu_mass_conservation_loss(pred_mat, training_steps)  
-            return L_data + L_phy + L_zm + L_deriv + L_neg + L_periodic + L_mass
-        end
-
-        adtype = Optimization.AutoForwardDiff()
-        optf = Optimization.OptimizationFunction((x, p) -> cpu_loss(x), adtype)
-        optprob = Optimization.OptimizationProblem(optf, p_NN)
-        losses = Float64[]
-        nn_history = Vector{Matrix{Float64}}()
-
-        callback = function (state, l)
-            pred = cpu_predict(state.u)
-            pred_mat = hcat(pred.u...)'
-            pred_norm = (pred_mat .- U_MEAN') ./ U_STD'
-            l_data = cpu_data_loss(pred_norm, data_norm, data_vars)
-            l_phy = cpu_physics_loss(pred_mat, training_steps, ode_params, physics_vars)
-            l_zm = cpu_zero_mean_loss(pred, state.u, zm_vars)
-            l_deriv = cpu_firstderiv_loss(pred_mat, target_data, deriv_vars)
-            l_neg = cpu_negativity_loss(pred_mat, neg_vars)
-            l_periodic = cpu_periodicity_loss(pred_mat)
-            l_mass = cpu_mass_conservation_loss(pred_mat, training_steps)
-            push!(losses, l_data + l_phy + l_zm + l_deriv + l_neg + l_periodic + l_mass)
-            push!(nn_history, cpu_compute_nn_contributions(pred, state.u))
-                println(
-                "Iter $(length(losses)) | " *
-                "Total: $(round(l_data + l_phy + l_zm + l_deriv + l_neg + l_periodic + l_mass, sigdigits=5)) | " *
-                "Data: $(round(l_data, sigdigits=5)) | " *
-                "Physics: $(round(l_phy, sigdigits=5)) | " *
-                "Zero Mean: $(round(l_zm, sigdigits=5)) | " *
-                "Deriv: $(round(l_deriv, sigdigits=5)) | " *
-                "Negativity: $(round(l_neg, sigdigits=5)) | " *
-                "Periodicity: $(round(l_periodic, sigdigits=5)) | " *
-                "Mass Cons: $(round(l_mass, sigdigits=5))"
-            )
-            if early_stopping &&
-                length(losses) > 50 &&
-                losses[end] - maximum(losses[(end-10):(end-1)]) > 0
-                println("Early stopping at iteration $(length(losses)) with loss $(losses[end])")
-                return true
-            else
-                return false
+    function compute_nn_contributions(sol, p_NN)
+        t = sol.t
+        u_mat = hcat(sol.u...)'  # (time × variables)
+        # Return (time × nn_vars) matrix instead of summing
+        nn_contrib = zeros(length(t), length(nn_vars))
+        for (k, i) in enumerate(nn_vars)
+            for ti in 1:length(t)
+                nn_contrib[ti, k] = nn_output_weight * nn(u_mat[ti, :], p_NN, st)[1][k]
             end
         end
+        return nn_contrib  # (time × nn_vars)
+    end
 
-        trained_params = Optimization.solve(
-            optprob,
-            optimizer(learning_rate),
-            callback = callback,
-            maxiters = iters,
+    ctx = (
+        pred_mat    = pred_mat,
+        pred_norm   = pred_norm,
+        data_norm   = data_norm,
+        training_steps = training_steps,
+        ode_params  = ode_params,
+        ode_problem = ode_problem,
+        nn_vars = nn_vars
         )
 
-        return (trained_params.u, st, losses, nn_history)    
+    adtype = Optimization.AutoForwardDiff()
+    optf = Optimization.OptimizationFunction((x, p) -> loss(active, ctx, config), adtype)
+    optprob = Optimization.OptimizationProblem(optf, p_NN)
+    losses = Float64[]
+    nn_history = Vector{Matrix{Float64}}()
 
-    elseif processor == "gpu"
-        nn_vars      === nothing && error("`nn_vars` must be specified")
-        data_vars    === nothing && error("`data_vars` must be specified")
-        physics_vars === nothing && error("`physics_vars` must be specified")
-
-        use_gpu = _gpu_available()
-        @info "GPU available: $use_gpu  (CUDA=$(CUDA.functional()))"
-        CUDA.functional()  && @info "CUDA device: $(CUDA.name(CUDA.device()))"
-
-        U_MEAN    = vec(mean(target_data, dims=1))
-        U_STD     = vec(std(target_data,  dims=1)) .+ 1e-6
-        data_norm = (target_data .- U_MEAN') ./ U_STD'
-
-        p_cpu, st_cpu = Lux.setup(rng, nn)
-        p_cpu  = ComponentVector{Float32}(p_cpu) .* 1f-3
-
-        p0     = p_cpu          # always CPU — solver and adjoint must stay on CPU
-        st_dev = use_gpu ? gdev(st_cpu) : st_cpu   # state can still go to GPU
-
-        function gpu_pinn_ode(u, p, t)
-            du_base = Zygote.ignore() do
-                du = zeros(Float64, length(u))
-                ode_problem.f(du, collect(Float64.(u)), ode_params, t)
-                du
-            end
-
-            u_in   = use_gpu ? gdev(Float32.(u)) : Float32.(u)
-            p_dev      = use_gpu ? gdev(p) : p
-            nn_out = nn(u_in, p_dev, st_dev)[1]
-            nn_out_cpu = use_gpu ? cdev(nn_out) : nn_out
-
-            n = length(u)
-            contrib = sum(
-                Float64.(Float32.(eachindex(u) .== nn_vars[k]) .* (nn_output_weight * nn_out_cpu[k]))
-                for k in 1:length(nn_vars)
-            )
-
-            return du_base .+ contrib
-        end
-
-        # ── ODE predict ────────────────────────────────────────────────────────
-        function gpu_predict(p)
-            p = use_gpu ? cdev(p) : p
-            prob = ODEProblem(
-                gpu_pinn_ode,
-                Float64.(ode_problem.u0),
-                ode_problem.tspan,
-                p,
-            )
-            sol = solve(
-                prob, 
-                Vern7(),
-                saveat   = training_steps,
-                dtmax    = dtmax,
-                reltol   = reltol,
-                abstol   = abstol,
-                sensealg = InterpolatingAdjoint(autojacvec=ZygoteVJP()),
-            )
-            return sol
-        end
-
-        # ── Loss helpers ───────────────────────────────────────────────────────
-        function gpu_data_loss(pred_mat)
-            pn = (pred_mat .- U_MEAN') ./ U_STD'
-            l = sum(mean(abs2, pn[:,j] .- data_norm[:,j]) for j in data_vars)
-            return data_weight * l
-        end
-        function gpu_deriv_loss(pred_mat, target_data, dt)
-            dpred   = diff(pred_mat[:, data_vars],           dims=1) ./ dt
-            dtarget = diff(target_data[:, data_vars],        dims=1) ./ dt  # ← slice target_data too!
-            return deriv_weight * mean(abs2, (dpred .- dtarget) ./ (U_STD[data_vars]' .+ 1e-6))
-        end 
-
-        function gpu_loss(p)
-            sol      = gpu_predict(p)
-            pred_mat = hcat(sol.u...)'
-            total = gpu_data_loss(pred_mat) # + gpu_deriv_loss(pred_mat, target_data, dt)
-            return total
-        end
-        
-
-        # ── Optimiser ──────────────────────────────────────────────────────────
-        adtype  = Optimization.AutoZygote()
-        optf    = Optimization.OptimizationFunction((x, _) -> gpu_loss(x), adtype)
-        optprob = Optimization.OptimizationProblem(optf, p0)
-
-        losses     = Float64[]
-        nn_history = Vector{Vector{Float64}}()
-        var_names  = ["pLV","pLA","psa","psv","Vlv","Vla","Qav","Qmv","Qs","Qsv"]
-
-        callback = function (state, _l)
-            sol      = gpu_predict(state)           # was state.u
-            pm       = Array(hcat(sol.u...)')
-            L_data   = gpu_data_loss(pm)
-            #dt       = Float64(training_steps.step)  
-            #L_deriv   = gpu_deriv_loss(pm, target_data, dt)
-            total    = L_data # + L_deriv
-            push!(losses, total)
-
-            p_dev   = use_gpu ? gdev(state) : state   # was state.u
-            contrib = zeros(length(nn_vars))
-            for ti in 1:size(pm, 1)
-                u_in = use_gpu ? gdev(Float32.(pm[ti,:])) : Float32.(pm[ti,:])
-                out  = use_gpu ? cdev(nn(u_in, gdev(p_dev), st_dev)[1]) : nn(u_in, p_dev, st_dev)[1]
-                for (k, _) in enumerate(nn_vars)
-                    contrib[k] += nn_output_weight * Float64(out[k])
-                end
-            end
-            push!(nn_history, contrib ./ size(pm, 1))
-
-            @printf("Iter %4d | Total: %.5e | Data: %.5e\n ", length(losses), total, L_data)
-
-            if plot_every > 0 && length(losses) % plot_every == 0
-                t    = sol.t
-                plts = [plot(t, pm[:,i], label="PINN", lw=2, title=var_names[i]) for i in 1:min(10, size(pm,2))]
-                for (j,i) in enumerate(data_vars)
-                    plot!(plts[i], t, target_data[:,j], label="Data", ls=:dash, lw=2)
-                end
-                display(plot(plts..., layout=(5,2), size=(900,800)))
-            end
-
-            if early_stopping && length(losses) > early_stop_warmup &&
-            losses[end] > maximum(losses[max(1,end-early_stop_window):end-1])
-                @info "Early stopping at iter $(length(losses))"
-                return true
-            end
+    callback = function (state, l)
+        pred = cpu_predict(state.u)
+        loss = loss(active, ctx, config)
+        push!(losses, loss)
+        push!(nn_history, cpu_compute_nn_contributions(pred, state.u))
+        if early_stopping &&
+            length(losses) > 50 &&
+            losses[end] - maximum(losses[(end-10):(end-1)]) > 0
+            println("Early stopping at iteration $(length(losses)) with loss $(losses[end])")
+            return true
+        else
             return false
         end
+    end
 
-        trained = Optimization.solve(optprob, optimizer(learning_rate),
-                                    callback=callback, maxiters=iters)
+    trained_params = Optimization.solve(
+        optprob,
+        optimizer(learning_rate),
+        callback = callback,
+        maxiters = iters,
+    )
 
-        return (trained.u, st_cpu, losses, nn_history)
-    else
-        error("Invalid processor specified: $processor. Use 'cpu' or 'gpu'.")
-    end # if 
-
+    return (trained_params.u, st, losses, nn_history) 
 end # function
-
-end # module PINNInfuser
+end # module
